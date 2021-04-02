7fd6c0fabefd2f93a5654f93c7ab7b946e8953b7
Thomas Dickmann <43370453+ThomasDickmann@users.noreply.github.com>
2021-04-02 09:34:59 +0200

    Rename Kalibrierkurven_Potentiometer.m to Calibration_curves_potentiometer.m

--- MATLAB_Models/Calibration_curves_potentiometer.m
+++ MATLAB_Models/Calibration_curves_potentiometer.m
@@ -6,15 +6,15 @@ yB = [0;5;10;15;20;25;30;35;40;45;47.5;50;55;60;65;70;75;80;85;90;95;100;105;110
 %ADC Werte an Winkelpositionen Poti B
 xB = [1453;1521;1575;1624;1682;1739;1790;1850;1916;1980;2100;2132;2200;2264;2323;2380;2440;2501;2559;2607;2657;2712;2771;2816;2877;2933;2975];
 
-%Skalierung der Werte für Annhäherung mit Polynomen höherer Ordnung
+%Skalierung der Werte fÃ¼r AnnhÃ¤herung mit Polynomen hÃ¶herer Ordnung
 %[coeffs_B, S, mu] = polyfit(xB, yB, 8)
-%fitted_yB = polyval(coeffs_B, xB, S, mu) %Gibt zurückskaliere Werte aus
+%fitted_yB = polyval(coeffs_B, xB, S, mu) %Gibt zurÃ¼ckskaliere Werte aus
 
-%Fitten eines Polynoms erster Ordnung, Error Estimation als 2. Rückgabewert
+%Fitten eines Polynoms erster Ordnung, Error Estimation als 2. RÃ¼ckgabewert
 pB = polyfit(xB,yB,1)
 
 %Auswertung des gefitteten Polynoms an den Stellen xB
-yB_fitted = polyval(pB,xB); %Gibt zurückskaliere Werte aus
+yB_fitted = polyval(pB,xB); %Gibt zurÃ¼ckskaliere Werte aus
 
 %Plot der fitted Kurve
 figure
@@ -39,12 +39,12 @@ SER_B = sqrt(sum(fehler_quad_B)/(length(yB)-2))
 %Plot der Residuen 
 %plot(xB, fehler_B,'r+', 'MarkerSize', 3)
 %grid on 
-%% Polyfitting für Sensor Gelenk K
+%% Polyfitting fÃ¼r Sensor Gelenk K
 
 %xK = [0; 20; 45; 60; 90; 120; 135; 160; 180]
 %yK = [349; 410; 471; 545; 640; 724; 766; 843; 907];
 
-%Kalibrierung 12 bit, 5° Schritte
+%Kalibrierung 12 bit, 5Â° Schritte
 yK = [10;15;20;25;30;35;40;45;50;55;60;62.5;65;70;75;80];
 xK = [1443;1496;1529;1594;1634;1689;1743;1811;1874;1930;1998;2136;2175;2242;2295;2367];
 
@@ -67,7 +67,7 @@ fehler_K = yK - polyval(pK,xK);
 fehler_quad_K = fehler_K.^2;
 SER_K = sqrt(sum(fehler_quad_K)/(length(yK)-2))
 
-%% Polyfitting für Sensor Gelenk A
+%% Polyfitting fÃ¼r Sensor Gelenk A
 
 %Wertepaare 10 bit 
 %xA = [-60; -45; -20; 0; 20; 45; 60; 90]; %Grad
